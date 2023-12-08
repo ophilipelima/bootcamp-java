902ced89281ee881e681610bfa32821cc20bf8a1
Philipe Lima <115307242+ophilipelima@users.noreply.github.com>
2023-12-08 11:27:39 -0300

    concluido

--- desafiosDeCodigo/JurosCompostos.java
+++ desafiosDeCodigo/JurosCompostos.java
@@ -2,7 +2,7 @@ package desafiosDeCodigo;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 import java.lang.Math;
-public class jurosCompostos {
+public class JurosCompostos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
